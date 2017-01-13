c17a1af0f3b9bb11eafd5ebaee006fd3aadcf405
fengye <fengye@huanleguang.com>
2017-01-13 11:38:32 +0800

    fix nvim in tmux clipboard bug on mac serria

--- alias.zsh
+++ alias.zsh
@@ -3,3 +3,7 @@ alias spacemacs="HOME=~/spacemacs emacs"
 alias ec="emacs -nw"
 alias ss="/usr/local/opt/shadowsocks-libev/bin/ss-local -c /usr/local/etc/shadowsocks-libev.json"
 alias proxy="proxychains4"
+
+# http://stackoverflow.com/a/39741226
+# clipboard-failure-in-tmux-vim-after-upgrading-to-macos-sierra
+alias nvim="reattach-to-user-namespace -l nvim"
