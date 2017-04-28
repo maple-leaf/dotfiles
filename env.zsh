ae5d78638dc732a877f91bae2de0c71b72593fc1
maple-leaf <tangjf.88@163.com>
2017-04-28 16:18:00 +0800

    remove code cmd because vscode provide way to add it to path

--- env.zsh
+++ env.zsh
@@ -8,14 +8,3 @@ export EDITOR='nvim'
 # variable required by neovim
 export XDG_DATA_HOME="$HOME/.config"
 
-# terimnal cmd for vscode
-function code {
-    if [[ $# = 0 ]]
-    then
-        open -a "Visual Studio Code"
-    else
-        local argPath="$1"
-        [[ $1 = /* ]] && argPath="$1" || argPath="$PWD/${1#./}"
-        open -a "Visual Studio Code" "$argPath"
-    fi
-}
