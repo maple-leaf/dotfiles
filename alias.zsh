1cf0bf48814a869e60ed1ce8d187d8765aea8567
maple-leaf <tangjf.88@163.com>
2017-09-13 20:38:08 +0800

    add gdp alias

--- alias.zsh
+++ alias.zsh
@@ -6,6 +6,8 @@ alias ss="/usr/local/opt/shadowsocks-libev/bin/ss-local -c /usr/local/etc/shadow
 alias proxy="/usr/local/Cellar/privoxy/3.0.26/sbin/privoxy /etc/privoxy/config"
 alias psqld="pg_ctl -D /usr/local/var/postgres start"
 
+alias gdp="git diff head~"
+
 # http://stackoverflow.com/a/39741226
 # clipboard-failure-in-tmux-vim-after-upgrading-to-macos-sierra
 #alias nvim="reattach-to-user-namespace -l nvim"
