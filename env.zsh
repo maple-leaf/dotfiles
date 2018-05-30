16114287811144256f0205433e97f8482b04d12b
maple-leaf <tangjf.88@163.com>
2018-05-30 17:49:07 +0800

    change PATH

--- env.zsh
+++ env.zsh
@@ -2,7 +2,8 @@ export GOPATH="$HOME/go"
 
 export ANDROID_HOME=/usr/local/opt/android-sdk
 
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$GOPATH/bin:$HOME/.composer/vendor/bin:$HOME/bin"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$GOPATH/bin:$HOME/.composer/vendor/bin:$HOME/bin:/Applications/Genymotion.app/Contents/MacOS/tools/"
+export PATH="$(yarn global bin):$PATH"
 
 export EDITOR='nvim'
 # variable required by neovim
