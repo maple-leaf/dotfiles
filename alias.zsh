56abcf7a2ee1a31ff82817c467408753ccd3a1ce
maple-leaf <tangjf.88@163.com>
2018-07-20 16:43:05 +0800

    feat: 🎸 change proxy alias to support yarn; add git-cz alias

--- alias.zsh
+++ alias.zsh
@@ -4,11 +4,13 @@ alias ec="emacs -nw"
 alias ss="/usr/local/opt/shadowsocks-libev/bin/ss-local -c /usr/local/etc/shadowsocks-libev.json"
 # alias proxy="proxychains4"
 alias myproxy="/usr/local/Cellar/privoxy/3.0.26/sbin/privoxy /etc/privoxy/config"
-alias proxy="export http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118"
+alias proxy="export http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 YARN_PROXY=http://127.0.0.1:8118"
 alias psqld="pg_ctl -D /usr/local/var/postgres start"
 alias cnpm="npm --registry=https://registry.npm.taobao.org"
 
+# git stuffs
 alias gdp="git diff head~"
+alias gcz="gitcz"
 
 # http://stackoverflow.com/a/39741226
 # clipboard-failure-in-tmux-vim-after-upgrading-to-macos-sierra
