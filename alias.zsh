5ed66767f882e48656f7e5387757b70c0f41ec44
maple-leaf <tangjf.88@163.com>
2018-09-01 08:55:58 +0800

    update alias

--- alias.zsh
+++ alias.zsh
@@ -5,12 +5,13 @@ alias ss="/usr/local/opt/shadowsocks-libev/bin/ss-local -c /usr/local/etc/shadow
 # alias proxy="proxychains4"
 alias myproxy="/usr/local/Cellar/privoxy/3.0.26/sbin/privoxy /etc/privoxy/config"
 alias proxy="export http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 YARN_PROXY=http://127.0.0.1:8118"
+alias unproxy="export http_proxy= https_proxy= YARN_PROXY="
 alias psqld="pg_ctl -D /usr/local/var/postgres start"
 alias cnpm="npm --registry=https://registry.npm.taobao.org"
 
 # git stuffs
 alias gdp="git diff head~"
-alias gcz="gitcz"
+alias gcz="git cz"
 
 # http://stackoverflow.com/a/39741226
 # clipboard-failure-in-tmux-vim-after-upgrading-to-macos-sierra
