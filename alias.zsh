13f0beff25bb326b13ca5dc59ba4eacfd9c194f8
maple-leaf <tangjf.88@163.com>
2018-11-23 14:48:09 +0800

    feat(universal-ctags): add computed property and method

--- alias.zsh
+++ alias.zsh
@@ -4,6 +4,7 @@ alias ec="emacs -nw"
 alias ss="/usr/local/opt/shadowsocks-libev/bin/ss-local -c /usr/local/etc/shadowsocks-libev.json"
 # alias proxy="proxychains4"
 alias myproxy="/usr/local/Cellar/privoxy/3.0.26/sbin/privoxy /etc/privoxy/config"
+alias hlgproxy="/usr/local/Cellar/privoxy/3.0.26/sbin/privoxy ~/proxy-config"
 alias proxy="export http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 YARN_PROXY=http://127.0.0.1:8118"
 alias unproxy="export http_proxy= https_proxy= YARN_PROXY="
 alias psqld="pg_ctl -D /usr/local/var/postgres start"
@@ -13,6 +14,12 @@ alias cnpm="npm --registry=https://registry.npm.taobao.org"
 alias gdp="git diff head~"
 alias gcz="git cz"
 
+# docker
+# get into container shell
+alias dockerIn=""
+# stop all container
+alias dockerStopAll="docker stop $(docker ps -aq)"
+
 # http://stackoverflow.com/a/39741226
 # clipboard-failure-in-tmux-vim-after-upgrading-to-macos-sierra
 #alias nvim="reattach-to-user-namespace -l nvim"
@@ -21,3 +28,6 @@ alias gcz="git cz"
 alias hs="python -m SimpleHTTPServer"
 
 alias jest-debug="node --inspect-brk node_modules/.bin/jest --runInBand"
+
+# list active pids for panels
+alias tmuxps="tmux list-panes -F '#{pane_active} #{pane_pid}'"
