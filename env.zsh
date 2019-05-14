fae37358e5576a7a5633087845a5eed356fc4459
maple-leaf <tangjf.88@163.com>
2019-05-14 10:23:33 +0800

    dont't load plugins for nvim when deal with merge

--- env.zsh
+++ env.zsh
@@ -5,7 +5,7 @@ export ANDROID_HOME=/usr/local/opt/android-sdk
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$GOPATH/bin:$HOME/.composer/vendor/bin:$HOME/bin:/Applications/Genymotion.app/Contents/MacOS/tools/"
 export PATH="$(yarn global bin):$PATH"
 
-export EDITOR='nvim'
+export EDITOR='nvim --noplugin'
 # variable required by neovim
 export XDG_DATA_HOME="$HOME/.config"
 
