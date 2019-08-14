d4e388a9134126ab8b8d79ae75789206493f0a60
maple-leaf <tangjf.88@163.com>
2019-08-14 18:17:24 +0800

    fix editor lanuch args

--- env.zsh
+++ env.zsh
@@ -5,7 +5,7 @@ export ANDROID_HOME=/usr/local/opt/android-sdk
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$GOPATH/bin:$HOME/.composer/vendor/bin:$HOME/bin:/Applications/Genymotion.app/Contents/MacOS/tools/"
 export PATH="$(yarn global bin):$PATH"
 
-export EDITOR='nvim --noplugin'
+export EDITOR='nvim -u NONE'
 # variable required by neovim
 export XDG_DATA_HOME="$HOME/.config"
 
